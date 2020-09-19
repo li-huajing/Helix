68c1722b38209f067cd5cc381e39a7d45b6a9b61
huajing.li <huajing.li@samsung.com>
2020-09-19 14:41:38 +0800

    add rules to filter useless data

--- v2/code/Model/ParseData.py
+++ v2/code/Model/ParseData.py
@@ -13,10 +13,33 @@ class ParseDataThread(threading.Thread):
 
     def run(self):
         if self.path[-4:] == '.csv':
-            data = pd.read_csv(self.path, low_memory=False)
+            data = pd.read_csv(self.path, index_col=0, low_memory=False)
         elif self.path[-5:] == '.xlsx':
-            data = pd.read_excel(self.path)
+            data = pd.read_excel(self.path, index_col=0)
         
+        '''
+        filter useless data
+        '''
+        # 1) delete the 'CDS' column value is NUll row data
+        data = data.drop(data[pd.isnull(data.CDS)].index)
+        # 2) delete the 'Gene1' column value is NUll row data
+        data = data.drop(data[pd.isnull(data.Gene1)].index)
+        # 3) delete the needless column data
+        labels = []
+        for item in data.keys():
+            if item[0:5] == 'Blank':
+                labels.append(item)
+            if item[-11:] == '_TargetGene':
+                labels.append(item)
+            if item[-8:] == '_Predict':
+                labels.append(item)
+            if item[-13:] == '_ZygoticState':
+                labels.append(item)
+        data = data.drop(columns=labels)
+
+        '''
+        get the candidate id
+        '''
         candidate = []
         for item in data.keys():
             if item[-6:] == '_Scale':

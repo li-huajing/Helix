b882ad05c0051fe366341db68ea1807aa4e2da76
huajing.li <huajing.li@samsung.com>
2020-09-24 18:59:17 +0800

    implement analyzing data by reference arithmetic

--- v2/code/Model/AnalyzeData.py
+++ v2/code/Model/AnalyzeData.py
@@ -15,15 +15,15 @@ class AnalyzeDataThread(Thread):
         '''
         self.data = param[0]
         self.path = param[1]
-        self.checkedId = param[2]
-        self.checkedDad = param[3]
-        self.checkedMom = param[4]
+        self.checkedId = param[2] + '_Scale'
+        self.checkedDad = str(int(param[2]) + 1) + '_Scale' if param[3] else ''
+        self.checkedMom = str(int(param[2]) + 2) + '_Scale' if param[4] else ''
         self.minScale = float(param[5])
         self.maxScale = float(param[6])
         self.minHori = float(param[7])
 
         # judge gender
-        checkIdAtColumnIndex = self.data.columns.get_loc(self.checkedId + '_Scale')
+        checkIdAtColumnIndex = self.data.columns.get_loc(self.checkedId)
         hori1AtColumnIndex = self.data.columns.get_loc('HoriAverage1')
         if checkIdAtColumnIndex > hori1AtColumnIndex:
             self.gender = 'female'
@@ -31,8 +31,22 @@ class AnalyzeDataThread(Thread):
             self.gender = 'male'
 
     def run(self):
+        # filter data
+        data = self.data.drop(['Chr Position Start', 'Chr Position End'], axis=1)
+        if self.gender == 'male':
+            horiKey = 'HoriAverage1'
+        else:
+            horiKey = 'HoriAverage2'
+        data = data.reset_index()
+        data = data.set_index('Gene1')
+        data = data[data[horiKey] > self.minHori]
+        data = data[(data[self.checkedId] < self.minScale) | (data[self.checkedId] > self.maxScale)]
+        self.filteredData = data
+
+        # arithmetic
         resultDictForTendency = self.getAbnormalGeneByTendency()
         resultDictForReference = self.getAbnormalGeneByReference()
+
         # work done
         self.resultSig.emit()
         self.isTaskDoneSig.emit(True)
@@ -73,18 +87,10 @@ class AnalyzeDataThread(Thread):
                         result.update({item:continousCdsIndex})
 
         #--------------------------------------------------------------
-        scaleKey = self.checkedId + '_Scale'
-        if self.gender == 'male':
-            horiKey = 'HoriAverage1'
-        else:
-            horiKey = 'HoriAverage2'
-
-        # filter data
-        data = self.data.reset_index()
-        data = data.set_index('Gene1')
-        data = data[data[horiKey] > self.minHori]
         result = {}
 
+        scaleKey = self.checkedId
+        data = self.filteredData
         lowScaleData = data[data[scaleKey] < self.minScale]
         highScaleData = data[data[scaleKey] > self.maxScale]
         abnormalScaleWithContinousCdsCount(lowScaleData)
@@ -94,4 +100,30 @@ class AnalyzeDataThread(Thread):
         return result
 
     def getAbnormalGeneByReference(self):
-        pass
+        # filter data of parent and self
+        data = self.filteredData
+        data = data.drop(self.checkedId, axis=1)
+        if self.checkedDad:
+            data = data.drop(self.checkedDad, axis=1)
+        if self.checkedMom:
+            data = data.drop(self.checkedMom, axis=1)
+        
+        referKey = []
+        for item in data.columns:
+            if item[-6:] == '_Scale':
+                referKey.append(item)
+        print(referKey)
+
+        for item in referKey:
+            data = data[(data[item] < self.maxScale) & (data[item] > self.minScale)]
+        print(data)
+
+        result = {}
+        for index, item in data.iterrows():
+            if index in result.keys():
+                result[index].append(item.Index)
+            else:
+                result.update({index:[item.Index]})
+
+        print(result)
+        return result
